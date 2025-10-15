a57cd7f0904317266b9d9c4f5b2d7e8ba90302a8
Nabeel Himaz <nabeelhimaz@gmail.com>
2025-10-15 20:11:22 +0100

    Initial commit: Add workspace with all labs

---
+++ labs/Lab1-Counter/counter_tb.cpp
@@ -0,0 +1,51 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char **argv, char **env)
+{
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vcounter *top = new Vcounter;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("counter.vcd");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 0;
+    top->en = 1;
+
+    // run simulation for many clock cycles
+    for (i = 0; i < 300; i++)
+    {
+
+        // dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++)
+        {
+            tfp->dump(2 * i + clk); // unit is in ps
+            top->clk = !top->clk;
+            top->eval();
+        }
+
+        if (i == 8)
+        {
+            top->en = 0;
+        }
+        if (i == 11)
+        {
+            top->en = 1;
+        }
+        // top->rst = !(i % 10); //| i < 9;
+
+        if (Verilated::gotFinish())
+            exit(0);
+    }
+    tfp->close();
+    exit(0);
+}
